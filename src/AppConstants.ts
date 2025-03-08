d0d7416051eab2b65fa457b9379dbe3225cdf1c2
Wilher Distu <wilherlt@gmail.com>
2025-03-08 07:21:14 -0300

    Adicionando as configurações do Vercel

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-  APP_ID: process.env.REACT_APP_APP_ID,
-  REGION: process.env.REACT_APP_REGION,
-  AUTH_KEY: process.env.REACT_APP_AUTH_KEY,
+  APP_ID: process.env.REACT_APP_APP_ID || "271513dd81037f96",
+  REGION: process.env.REACT_APP_REGION || "US",
+  AUTH_KEY: process.env.REACT_APP_AUTH_KEY || "59dc31e2a84b8a95bf6118f181cc7575add978f5",
 };
